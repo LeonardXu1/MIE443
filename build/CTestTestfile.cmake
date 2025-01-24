c5d2b8ccf70fc0847c5b7d6b8a71f013fe6200ad
LeonardXu1 <leonard.xu@mail.utoronto.ca>
2025-01-24 11:00:57 -0500

    untracking files

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo/catkin_ws/src
-# Build directory: /home/leo/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(beginner_tutorials)
-subdirs(MIE443/mie443_contest_1)
--- build/MIE443/mie443_contest_1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo/catkin_ws/src/MIE443/mie443_contest_1
-# Build directory: /home/leo/catkin_ws/build/MIE443/mie443_contest_1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
