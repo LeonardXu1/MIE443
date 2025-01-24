8cdaabd84fec734e37ac171cbc8955366288d592
LeonardXu1 <leonard.xu@mail.utoronto.ca>
2025-01-24 10:54:56 -0500

    initial

---
+++ build/MIE443/mie443_contest_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leo/catkin_ws/src/MIE443/mie443_contest_1
+# Build directory: /home/leo/catkin_ws/build/MIE443/mie443_contest_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
