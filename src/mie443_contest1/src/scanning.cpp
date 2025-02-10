f6a2aa0966c3b2783f96777d6f48c0838b3a27ea
matiou8180 <matiou.chan@gmail.com>
2025-02-10 03:12:24 -0500

    close edge scanning algorithm complete

--- src/mie443_contest1/src/scanning.cpp
+++ src/mie443_contest1/src/scanning.cpp
@@ -8,8 +8,11 @@ float maxLaserAngle;
 float finalLaserDist;
 float finalLaserAngle;
 
+float obsLaserAngle;
+float obsLaserDist;
+
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
-{
+{   
     dat.angle_min = msg->angle_min;
     dat.angle_max = msg->angle_max;
     dat.angle_increment = msg->angle_increment;
@@ -34,56 +37,122 @@ void processLaserData()
     maxLaserDist = 0;
     maxLaserAngle = 0.0;
     int maxLaserIdx = -1;               //set to -1 to be a placeholder for "no valid index"
-    float robotWidth = 10;               //  must change this depending on units and actual width. also currently unsure if rounding will be an error 
+    float robotWidth = .4;               //  must change this depending on units and actual width. also currently unsure if rounding will be an error 
     
     // Variables for obstacle detection and path compensation
-    float obsLaserAngle;
-    float obsLaserDist;
-    int obsLaserIdx;
-    float obsClearance = robotWidth / 2 + 0.05;
+    int rightObsLaserIdx;
+    obsLaserAngle = 0;
+    obsLaserDist = 0;
+    int leftObsLaserIdx;
+    float obsClearance = robotWidth / 2 + 0.1;
+    
+    //testing can delete later
+    float leftLaser, rightLaser, LLlaser;
+
+    ROS_INFO("Laser scan size: %lu", dat.ranges.size());
+    ROS_INFO("0 index distance:  %0.2f  | 640 index distance %0.2f", dat.ranges[0], dat.ranges[630]);
+
+    //test to print all laser data values
+    // ROS_INFO("Printing dat.ranges:");
+    // for (size_t i = 0; i < dat.ranges.size(); i++) {
+    //     ROS_INFO("dat.ranges[%lu] = %0.2f", i, dat.ranges[i]);
+    // }
+
+    for (uint32_t laser_idx = 0; laser_idx < dat.nLasers; ++ laser_idx){        //cycle through all laser distances and store the max distance and corresponding index, as well as index's angle
+        if (dat.ranges[laser_idx] > maxLaserDist){
+            maxLaserDist = dat.ranges[laser_idx];
+            maxLaserIdx = laser_idx;
+            maxLaserAngle = dat.angle_min + maxLaserIdx * dat.angle_increment;
+            
+            //testing
+            leftLaser = dat.ranges[maxLaserIdx + 1];
+            LLlaser = dat.ranges[maxLaserIdx + 4];
+
+            rightLaser = dat.ranges[maxLaserIdx - 1];
+
+        }
+    }
+    //Print the distances around maxlaser index
+    for (int i = maxLaserIdx - 10; i <= maxLaserIdx + 10; ++i) {
+        if (i >= 0 && i < dat.ranges.size()) {  // Check if within bounds
+            ROS_INFO("dat.ranges[%d] = %0.2f", i, dat.ranges[i]);
+        }
+    }
+
+    ROS_INFO("maxLaserIdx: %d | maxLaserDist: %0.2f | leftdist %0.2f | rightdist %0.2f  | more left dist %0.2f", maxLaserIdx, maxLaserDist, leftLaser, rightLaser, LLlaser);
+    
+    //RIGHT OBS CHECK --> First check value is not NaN
+    rightObsLaserIdx = maxLaserIdx - 1;
+    ROS_INFO("right wall indx %d", rightObsLaserIdx);
+    while (std::isnan(dat.ranges[rightObsLaserIdx]) && rightObsLaserIdx > 0) {
+        rightObsLaserIdx--; // Move to the previous index
+    }
+    ROS_INFO("right wall indx %d", rightObsLaserIdx);
 
 
+    //LEFT OBS CHECK --> NaN Check
+    leftObsLaserIdx = maxLaserIdx + 1;
+    ROS_INFO("left wall indx %d", leftObsLaserIdx);
+    while (std::isnan(dat.ranges[leftObsLaserIdx]) && leftObsLaserIdx < dat.nLasers) {
+        leftObsLaserIdx++; // Move to the previous index
+    }
+    ROS_INFO("left wall indx %d", leftObsLaserIdx);
 
-    for (uint32_t laser_idx = 0; laser_idx < dat.nLasers; ++ laser_idx)
-        {        //cycle through all laser distances and store the max distance and corresponding index, as well as index's angle
-            if (dat.ranges[laser_idx] > maxLaserDist){
-                maxLaserDist = dat.ranges[laser_idx];
-                maxLaserIdx = laser_idx;
-                maxLaserAngle = dat.angle_min + maxLaserIdx * dat.angle_increment;
+    
+    //RIGHT OBS CHECK --> then complete calc with valid value
+    if ((dat.ranges[maxLaserIdx] - dat.ranges[rightObsLaserIdx] > 0.2 )) {       // in other words: if the obstacle is on the right of the maxlaserdist 
+    // if (dat.ranges[maxLaserIdx] + dat.ranges[maxLaserIdx - 1] > 0.2 ){
+        float test_= dat.ranges[maxLaserIdx] - dat.ranges[maxLaserIdx - 1];
+        ROS_INFO("obstacle on right");
+        obsLaserDist = dat.ranges[rightObsLaserIdx];
+        ROS_INFO("obstacle idx %0.2d", rightObsLaserIdx);            //TEST
 
-                finalLaserAngle = maxLaserAngle;
+        obsLaserAngle = atan(obsClearance / obsLaserDist);
+        finalLaserAngle = maxLaserAngle + obsLaserAngle;                        //this is the case for both positive and negative maxlaserAngle cases. 
+    
+        ROS_INFO("max angle: %0.2f  | correction angle: %0.2f   |   final angle: %0.2f", maxLaserAngle, obsLaserAngle, finalLaserAngle);
+    }
 
-            }
-            if (dat.ranges[maxLaserIdx] - dat.ranges[maxLaserIdx - 1] > 0.1 ){          // in other words: if the obstacle is on the left of the maxlaserdist 
-                obsLaserIdx = maxLaserIdx - 1;
-                obsLaserDist = dat.ranges[obsLaserIdx];
-                obsLaserAngle = atan(obsClearance / obsLaserDist);
-                finalLaserAngle = maxLaserAngle - obsLaserAngle;                        //this is the case for both positive and negative maxlaserAngle cases. 
-            }
-                
-            if (dat.ranges[maxLaserIdx] - dat.ranges[maxLaserIdx + 1] > 0.1 ){          // in other words: if the obstacle is on the left of the maxlaserdist 
-                obsLaserIdx = maxLaserIdx + 1;
-                obsLaserDist = dat.ranges[obsLaserIdx];
-                obsLaserAngle = atan(obsClearance / obsLaserDist);
-                finalLaserAngle = maxLaserAngle + obsLaserAngle;                        //this is the case for both positive and negative maxlaserAngle cases. 
-            }
+
+    //LEFT OBS CHECK --> Calcs
+    else if ((dat.ranges[maxLaserIdx] - dat.ranges[leftObsLaserIdx] > 0.2 )){          // in other words: if the obstacle is on the left of the maxlaserdist 
+        ROS_INFO("obstacle on left");                           //TEST
+        
+        obsLaserDist = dat.ranges[leftObsLaserIdx];
+        
+        ROS_INFO("obstacle idx %0.2d", leftObsLaserIdx);            //TEST
+
+        obsLaserAngle = atan(obsClearance / obsLaserDist);
+        finalLaserAngle = maxLaserAngle - obsLaserAngle;                        //this is the case for both positive and negative maxlaserAngle cases. 
+
+        ROS_INFO("max angle: %0.2f  | correction angle: %0.2f   |   final angle: %0.2f", maxLaserAngle, obsLaserAngle, finalLaserAngle);
+
+    }
+
+    else{
+        finalLaserAngle = maxLaserAngle;
         }
 }
 
 void scanningBehaviour(){
     bool taskComplete;
     int step = getStep();
-
     if(step == 0){
+        if (dat.ranges.size() != 0){
+            takeStep();
+        }
+    }
+
+    else if(step == 1){
         processLaserData();
-        ROS_INFO("max angle: %0.2f  | max dist: %0.2f", maxLaserAngle, maxLaserDist);
+        ROS_INFO("max angle: %0.2f  | correction angle: %0.2f   |   final angle: %0.2f", maxLaserAngle, obsLaserAngle, finalLaserAngle);
         takeStep();
     }
-    else if(step == 1){
+    else if(step == 2){
         savePos();
         takeStep();
     }
-    else if(step ==  2){
+    else if(step ==  3){
         if (maxLaserAngle < 0){
             taskComplete = moveAngle(-finalLaserAngle, SLOW_ANGULAR, CW);
         }
@@ -94,12 +163,12 @@ void scanningBehaviour(){
             takeStep();
         }
     }
-    else if(step == 3){
+    else if(step == 4){
         savePos();
         takeStep();
     }
-    else if(step == 4){
-        moveLinearSpeed(SLOW_LINEAR, FORWARD);
+    else if(step == 5){
+        moveLinearSpeed(FAST_LINEAR, FORWARD);
         
         // if(taskComplete){
         //     takeStep();
