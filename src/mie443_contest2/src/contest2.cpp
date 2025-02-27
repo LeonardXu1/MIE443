90ace71e1bcb6eabb3e22a70312d1b21fbbe23e9
NathanielE <eichhornnathaniel@gmail.com>
2025-02-27 09:43:33 -0800

    implemented user input to move robot

--- src/mie443_contest2/src/contest2.cpp
+++ src/mie443_contest2/src/contest2.cpp
@@ -4,6 +4,28 @@
 #include <imagePipeline.h>
 #include <chrono>
 
+#include <iostream>
+
+std::vector<float> positionInput(){
+    float x, y, phi;
+
+    std::cout << "Enter x goal: ";
+
+    std::cin >> x;
+
+    std::cout << "Enter y goal: ";
+    
+    std::cin >> y;
+
+    std::cout << "Enter phi goal: ";
+
+    std::cin >> phi;
+
+    std::vector<float> inputPos = {x, y, phi};
+
+    return inputPos;
+}
+
 int main(int argc, char** argv) {
     // Setup ROS.
     ros::init(argc, argv, "contest2");
@@ -25,10 +47,26 @@ int main(int argc, char** argv) {
     // Initialize image objectand subscriber.
     ImagePipeline imagePipeline(n);
 
+    // Initialize navigation
+    Navigation nav;
+
     // contest count down timer
     std::chrono::time_point<std::chrono::system_clock> start;
     start = std::chrono::system_clock::now();
     uint64_t secondsElapsed = 0;
+
+    bool reached;
+
+    // std::vector<float> box1 = boxes.coords[1];
+    // float x = box1[0];
+    // float y = box1[1];
+    // float phi = box1[2];
+
+    std::vector<float> inputPos = positionInput();
+
+    float x = inputPos[0];
+    float y = inputPos[1];
+    float phi = inputPos[2];
     
     // Execute strategy.
     while(ros::ok() && secondsElapsed <= 300) {
@@ -36,7 +74,27 @@ int main(int argc, char** argv) {
         /***YOUR CODE HERE***/
         // Use: boxes.coords
         // Use: robotPose.x, robotPose.y, robotPose.phi
-        imagePipeline.getTemplateID(boxes);
+
+        ROS_INFO("Robot Position:");
+        ROS_INFO("x: %f y: %f phi: %f", robotPose.x, robotPose.y, robotPose.phi);
+        
+        if(!reached){
+            ROS_INFO("Goal:");
+            ROS_INFO("x: %f y: %f phi: %f", x, y, phi);
+    
+            reached = nav.moveToGoal(x, y, phi);
+        }
+        else{
+            inputPos = positionInput();
+
+            x = inputPos[0];
+            y = inputPos[1];
+            phi = inputPos[2];
+
+            reached = false;
+        }
+
+        // imagePipeline.getTemplateID(boxes);
         ros::Duration(0.01).sleep();
     }
     return 0;
